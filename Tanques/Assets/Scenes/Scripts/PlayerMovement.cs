65e4e4e25d6a580fbb68a172fffae8dd2ce74a84
WilliamMRibeiro <55996790+WilliamMRibeiro@users.noreply.github.com>
2019-10-18 16:29:19 -0300

    continua

---
+++ Tanques/Assets/Scenes/Scripts/PlayerMovement.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float speed = 1;
+
+    private Rigidbody rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+
+        // float n_Speed = moveVertical * speed * Time.deltaTime;
+
+        //Vector3 movement = new Vector3(0, 0, n_Speed);
+
+        rb.MovePosition(rb.position + movement);
+    }
+}
+
