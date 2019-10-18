08772f8df4979bccc6dc7ccf40a0a1dc29f36a72
WilliamMRibeiro <55996790+WilliamMRibeiro@users.noreply.github.com>
2019-10-18 17:01:20 -0300

    update

--- Tanques/Assets/Scenes/Scripts/PlayerMovement.cs
+++ Tanques/Assets/Scenes/Scripts/PlayerMovement.cs
@@ -18,11 +18,26 @@ public class PlayerMovement : MonoBehaviour
         float moveHorizontal = Input.GetAxis("Horizontal");
         float moveVertical = Input.GetAxis("Vertical");
 
-        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+        //Movimentacao(moveVertical);
 
+        
+        Vector3 movement = new Vector3(0, 0.0f,moveVertical);
+        
         // float n_Speed = moveVertical * speed * Time.deltaTime;
 
         //Vector3 movement = new Vector3(0, 0, n_Speed);
+
+        //rb.MovePosition(rb.position + movement);
+        rb.MovePosition(rb.position + movement * speed * Time.deltaTime);
+
+        Quaternion rot = Quaternion.Euler(new Vector3(0, moveHorizontal, 0));
+
+        rb.MoveRotation(rb.rotation * rot);
+    }
+
+    void Movimentacao(float vertical)
+    {
+        Vector3 movement = transform.forward * vertical * speed * Time.deltaTime;
 
         rb.MovePosition(rb.position + movement);
     }
