5985a28d794745cb92feb43796305d94d256f788
WilliamMRibeiro <55996790+WilliamMRibeiro@users.noreply.github.com>
2019-10-19 16:03:37 -0300

    avanços

---
+++ Tanques/Assets/Scenes/Scripts/CannonHorizontal.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CannonHorizontal : MonoBehaviour
+{
+    public float speed = 10;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        /*float mouseX = Input.GetAxis("Mouse X") * speed * Time.deltaTime;
+        float mouseY = Input.GetAxis("Mouse Y") * speed * Time.deltaTime;
+
+        transform.Rotate(0, mouseX, 0);*/
+
+        Vector3 horizontal = new Vector3(0, speed * Time.deltaTime, 0);
+
+        if (Input.GetKey(KeyCode.Keypad4))
+        {
+            transform.Rotate(-horizontal);
+
+        }
+        if (Input.GetKey(KeyCode.Keypad6))
+        {
+            transform.Rotate(horizontal);
+        }
+
+        
+    }
+}
--- Tanques/Assets/Scenes/Scripts/PlayerMovement.cs
+++ Tanques/Assets/Scenes/Scripts/PlayerMovement.cs
@@ -4,39 +4,21 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    public float speed = 10;
+    public float speed = 10.0f;
+    public float rotatespeed = 50.0f;
+    
 
-    private Rigidbody rb;
-
-    void Start()
-    {
-        rb = GetComponent<Rigidbody>();
-    }
-
+    
+    
     void FixedUpdate()
     {
-        float moveHorizontal = Input.GetAxis("Horizontal");
-        float moveVertical = Input.GetAxis("Vertical");
+        float rotateTank = Input.GetAxis("Horizontal");
+        float moveTank = Input.GetAxis("Vertical");
 
-        //Movimentacao(moveVertical);
+        GetComponent<Rigidbody>().velocity = transform.forward * speed * moveTank;
 
-        
-       // Vector3 movement = new Vector3(0, 0.0f,moveVertical);
-        Vector3 rotation = transform.forward * moveVertical * speed * Time.deltaTime;
-
-        //rb.MovePosition(rb.position + movement);
-        rb.MovePosition(rb.position - rotation * speed * Time.deltaTime);
-
-        Quaternion rot = Quaternion.Euler(new Vector3(0, moveHorizontal, 0));
-
-        rb.MoveRotation(rb.rotation * rot);
+        transform.Rotate(Vector3.up * rotatespeed * rotateTank * Time.deltaTime);
     }
-
-   // void Movimentacao(float vertical)
-   // {
-        //Vector3 movement = transform.forward * vertical * speed * Time.deltaTime;
-
-        //rb.MovePosition(rb.position + movement);
-    //}
+       
 }
 
