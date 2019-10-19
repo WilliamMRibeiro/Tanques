2a7072934541e4cc21509f845ff66bc6bc56b149
WilliamMRibeiro <55996790+WilliamMRibeiro@users.noreply.github.com>
2019-10-19 02:51:17 -0300

    andou

--- Tanques/Assets/Scenes/Scripts/PlayerMovement.cs
+++ Tanques/Assets/Scenes/Scripts/PlayerMovement.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    public float speed = 1;
+    public float speed = 10;
 
     private Rigidbody rb;
 
@@ -21,25 +21,22 @@ public class PlayerMovement : MonoBehaviour
         //Movimentacao(moveVertical);
 
         
-        Vector3 movement = new Vector3(0, 0.0f,moveVertical);
-        
-        // float n_Speed = moveVertical * speed * Time.deltaTime;
-
-        //Vector3 movement = new Vector3(0, 0, n_Speed);
+       // Vector3 movement = new Vector3(0, 0.0f,moveVertical);
+        Vector3 rotation = transform.forward * moveVertical * speed * Time.deltaTime;
 
         //rb.MovePosition(rb.position + movement);
-        rb.MovePosition(rb.position + movement * speed * Time.deltaTime);
+        rb.MovePosition(rb.position - rotation * speed * Time.deltaTime);
 
         Quaternion rot = Quaternion.Euler(new Vector3(0, moveHorizontal, 0));
 
         rb.MoveRotation(rb.rotation * rot);
     }
 
-    void Movimentacao(float vertical)
-    {
-        Vector3 movement = transform.forward * vertical * speed * Time.deltaTime;
+   // void Movimentacao(float vertical)
+   // {
+        //Vector3 movement = transform.forward * vertical * speed * Time.deltaTime;
 
-        rb.MovePosition(rb.position + movement);
-    }
+        //rb.MovePosition(rb.position + movement);
+    //}
 }
 
