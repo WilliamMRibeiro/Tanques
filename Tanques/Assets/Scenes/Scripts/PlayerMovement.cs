92268a21f309319288a12e631e49b2e5dc264c9f
WilliamMRibeiro <55996790+WilliamMRibeiro@users.noreply.github.com>
2019-10-21 16:47:08 -0300

    spawn

--- Tanques/Assets/Scenes/Scripts/PlayerMovement.cs
+++ Tanques/Assets/Scenes/Scripts/PlayerMovement.cs
@@ -6,16 +6,21 @@ public class PlayerMovement : MonoBehaviour
 {
     public float speed = 10.0f;
     public float rotatespeed = 50.0f;
-    
 
-    
-    
+    Rigidbody rb;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+
     void FixedUpdate()
     {
         float rotateTank = Input.GetAxis("Horizontal");
         float moveTank = Input.GetAxis("Vertical");
 
-        GetComponent<Rigidbody>().velocity = transform.forward * speed * moveTank;
+        rb.velocity = transform.forward * speed * moveTank *Time.deltaTime;
 
         transform.Rotate(Vector3.up * rotatespeed * rotateTank * Time.deltaTime);
     }
---
+++ Tanques/Assets/Scenes/Scripts/SpawnEnemy.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnEnemy : MonoBehaviour
+{
+    public GameObject enemy;
+    private float timeLimit = 5f;
+    private float timer = 0f;
+    public float speed = 10.0f;
+    public float rotatespeed = 50.0f;
+    public Transform saida;
+    Rigidbody rb;
+    // Start is called before the first frame update
+
+
+    // Update is called once per frame
+    private void Awake()
+    {
+        //rb = GetComponent<Rigidbody>();
+    }
+    void Update()
+    {
+        timer += Time.deltaTime;
+
+        if (timer >= timeLimit)
+        {
+            
+            
+                float rotateTank = 10f;
+                float moveTank = 50f;
+
+                GameObject instaenemy = Instantiate(enemy, saida.position, saida.rotation) as GameObject;
+                Rigidbody instenemytrb = instaenemy.GetComponent<Rigidbody>();
+                rb.velocity = transform.forward * speed * moveTank * Time.deltaTime;
+
+                transform.Rotate(Vector3.up * rotatespeed * rotateTank * Time.deltaTime);
+            
+            timer = 0;
+            Destroy(gameObject);
+        }
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+
+        if (other.gameObject.tag == "Bullet")
+        {
+
+            other.gameObject.SetActive(false);
+            Destroy(gameObject);
+
+
+        }
+
+
+
+    }
+    
+}
